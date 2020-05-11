e71ea412aab04071ceb1ea61f93b144c5557efee
Martin Iglič <martin.iglic@gmail.com>
2020-05-11 18:25:33 +0200

    pisanje nadaljnih metod

--- model.py
+++ model.py
@@ -5,15 +5,37 @@ NAPACNA_CRKA = '-'
 ZMAGA = 'W'
 PORAZ = 'X'
 
+bazen_besed =[]
+
+with open('vislive/besede.txt') as datoteka_bazena:
+    for beseda in datoteka_bazena:
+        bazen_besed.append(beseda.strip().lower())
 class igra:
 
-    def __init__(self, geslo, crke):
+    def __init__(self, geslo, crke=None):
         self.geslo = geslo
-        self.crke = crke
+        if crke is None:
+            self.crke = []
+        else:
+            self.crke = crke
 
 
-    def napacne_crke(self, crke):
+    def napacne_crke(self):
         napacne = []
-        for 
+        for crka in crke:
+            if crka not in geslo:
+                napacne.append(crka)
+        return napacne
 
     def pravilne_crke(self):
+        pravilne = []
+        for crka in crke:
+            if crka in geslo:
+                pravilne.append(crka)
+        return pravilne
+
+    def stevilo_napak(self):
+        return len(napacne_crke(geslo, crke))
+
+    def zmaga(geslo):
+
